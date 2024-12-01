bd0d194565cac1a7d20170bdf0f9c4d433b74090
CaioBoh <caioboh@hotmail.com>
2024-12-01 20:43:31 -0300

    :bug: fix: Ajuste nome função

--- src/controllers/schools.py
+++ src/controllers/schools.py
@@ -1,9 +1,9 @@
-from src.models.schools import get_all_schools, get_school_by_city, get_cities
+from src.models.schools import get_all_schools, get_schools_dashboard
 
 # Função para listar todas as cidades
 def list_schools(filters=None):
     return get_all_schools(filters)
 
 # Função para listar escola por cidade
-def list_schools():
-    return get_school()
+def list_school_dashboard():
+    return get_schools_dashboard()
