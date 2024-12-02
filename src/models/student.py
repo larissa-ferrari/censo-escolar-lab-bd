23aaa435925ce58c22d16b1c134b0aecf3d46b45
Jeferson-Filho <jpdietrichfilho.2015@gmail.com>
2024-12-01 22:04:18 -0300

    :bug: fix: get_number_students_by_level Etapa_Ensino function

--- src/models/student.py
+++ src/models/student.py
@@ -6,10 +6,15 @@ def get_number_students_by_level():
     try:
         with connection.cursor(dictionary=True) as cursor:
             query = """
-                SELECT TP_ETAPA_ENSINO, SUM(NU_MATRICULAS) as SUM_MATRICULAS
-                FROM turma
-                GROUP BY TP_ETAPA_ENSINO
-                ORDER BY SUM_MATRICULAS DESC;
+                SELECT 
+                    Etapa_Ensino(TP_ETAPA_ENSINO),
+                    SUM(NU_MATRICULAS) as SUM_MATRICULAS
+                FROM
+                    turma
+                GROUP BY
+                    TP_ETAPA_ENSINO
+                ORDER BY
+                    SUM_MATRICULAS DESC;
             """
             cursor.execute(query)
             return cursor.fetchall()
