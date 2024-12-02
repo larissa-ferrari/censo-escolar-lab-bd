1e37f801239cc59ac269ef0b9fc539e07fa69843
CaioBoh <caioboh@hotmail.com>
2024-12-01 23:01:57 -0300

    :sparkles: feat: dashboard turmas

--- src/controllers/turmas.py
+++ src/controllers/turmas.py
@@ -1,4 +1,4 @@
-from src.models.turmas import get_class_by_school_id
+from src.models.turmas import get_class_by_school_id, get_all_classes
 
 def list_all_classes(filters=None):
     return get_all_classes(filters)
