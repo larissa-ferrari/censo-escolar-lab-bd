e1f1fc0c8d1f361760756d19e26428dd30c623f6
CaioBoh <caioboh@hotmail.com>
2024-12-01 23:15:28 -0300

    :sparkles: feat: ideb

--- src/controllers/ideb.py
+++ src/controllers/ideb.py
@@ -2,6 +2,5 @@ from src.models.ideb import get_ideb_grades_by_school
 
 # Função para buscar o número de alunos por nível de ensino
 def list_ideb_grades_by_school():
-    df = get_ideb_grades_by_school()    
-    return df
+    return get_ideb_grades_by_school()    
 
