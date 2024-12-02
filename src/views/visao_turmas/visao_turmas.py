46af34a71110de9d9cf2f69b07f6b5986a893d9c
CaioBoh <caioboh@hotmail.com>
2024-12-01 22:36:55 -0300

    :sparkles: feat: dashboard turmas com disciplinas

--- src/views/visao_turmas/visao_turmas.py
+++ src/views/visao_turmas/visao_turmas.py
@@ -27,9 +27,48 @@ if not schools_df.empty:
             "ID_TURMA": "Código",
             "NO_TURMA": "Nome"
         })
+
+        # Definir as colunas de disciplinas
+        disciplinas = [
+            "IN_DISC_QUIMICA",
+            "IN_DISC_FISICA",
+            "IN_DISC_MATEMATICA",
+            "IN_DISC_BIOLOGIA",
+            "IN_DISC_CIENCIAS",
+            "IN_DISC_LINGUA_PORTUGUESA",
+            "IN_DISC_LINGUA_INGLES",
+            "IN_DISC_LINGUA_ESPANHOL",
+            "IN_DISC_LINGUA_FRANCES",
+            "IN_DISC_LINGUA_OUTRA",
+            "IN_DISC_LINGUA_INDIGENA",
+            "IN_DISC_ARTES",
+            "IN_DISC_EDUCACAO_FISICA",
+            "IN_DISC_HISTORIA",
+            "IN_DISC_GEOGRAFIA",
+            "IN_DISC_FILOSOFIA",
+            "IN_DISC_ENSINO_RELIGIOSO",
+            "IN_DISC_ESTUDOS_SOCIAIS",
+            "IN_DISC_SOCIOLOGIA",
+            "IN_DISC_EST_SOCIAIS_SOCIOLOGIA",
+            "IN_DISC_INFORMATICA_COMPUTACAO",
+            "IN_DISC_PROFISSIONALIZANTE",
+            "IN_DISC_ATENDIMENTO_ESPECIAIS",
+            "IN_DISC_DIVER_SOCIO_CULTURAL",
+            "IN_DISC_LIBRAS",
+            "IN_DISC_PEDAGOGICAS",
+            "IN_DISC_OUTRAS"
+        ]
+
+        # Criar a coluna com os nomes das disciplinas onde o valor é True
+        def get_disciplines(row):
+            # Filtrar as disciplinas True e retornar os nomes
+            return ', '.join([disciplina.replace("IN_DISC_", "").replace("_", " ") for disciplina in disciplinas if row[disciplina]])
+
+        df['Disciplinas'] = df.apply(get_disciplines, axis=1)
+
         df = df.reset_index(drop=True)
 
-        st.dataframe(df, use_container_width=True)
+        st.dataframe(df[['Código', 'Nome', 'Disciplinas']], use_container_width=True)
     else:
         st.info(f"Nenhuma Turma Encontrada para a Escola {selected_school}")
 else:
