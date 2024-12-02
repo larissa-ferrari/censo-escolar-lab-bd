2ed7c0b552270fd860aadd9aa3979fd24d4c6b4a
CaioBoh <caioboh@hotmail.com>
2024-12-01 22:21:06 -0300

    :bug: fix: Ajuste query

--- src/controllers/turmas.py
+++ src/controllers/turmas.py
@@ -1,4 +1,4 @@
 from src.models.turmas import get_class_by_school_id
 
-def list_class(filters=None):
-    return get_class_by_school_id(filters)
+def list_class(school_name):
+    return get_class_by_school_id(school_name)
