0cc616d96608ea7f69ac3fbb2b865930ecc0441a
Jeferson-Filho <jpdietrichfilho.2015@gmail.com>
2024-12-01 23:33:12 -0300

    :sparkles: feat: nova view escola e quantidade alunos, prof, turmas

--- src/controllers/schools.py
+++ src/controllers/schools.py
@@ -1,4 +1,4 @@
-from src.models.schools import get_all_schools, get_schools_dashboard, get_schools_qtd_dashboard, get_teachers_students
+from src.models.schools import get_all_schools, get_schools_dashboard, get_schools_qtd_dashboard, get_schools_qtd_dashboard_with_docente, get_teachers_students
 
 # Função para listar todas as escolas
 def list_schools(filters=None):
@@ -8,9 +8,13 @@ def list_schools(filters=None):
 def list_school_dashboard():
     return get_schools_dashboard()
 
-# Função para listar quantidades nas escolas
+# Função para listar quantidades nas escolas (sem docente)
 def list_school_qtd_dashboard():
     return get_schools_qtd_dashboard()
+
+# Função para listar quantidades nas escolas (com docente)
+def list_school_qtd_dashboard():
+    return get_schools_qtd_dashboard_with_docente()
 
 # Função para listar os professores e alunos
 def list_teachers_students(school_name):
--- src/views/visao_escola/visao_escola_docente.py
+++ src/views/visao_escola/visao_escola_docente.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from src.controllers.schools import get_schools_qtd_dashboard
+from src.controllers.schools import get_schools_qtd_dashboard_with_docente
 import pandas as pd
 
 st.title("Escolas da Cidade")
 st.subheader("Visão da Escola")
     
 with st.spinner("Carregando Escolas..."):
-    bookmarks = get_schools_qtd_dashboard()
+    bookmarks = get_schools_qtd_dashboard_with_docente()
 
 df = pd.DataFrame(bookmarks)
 
@@ -16,6 +16,7 @@ else:
     df = df.rename(columns={
         "CO_ENTIDADE": "Código",
         "NO_ENTIDADE": "Nome",
+        "Docentes": "Qtd. Professores",
         "Matricula": "Qtd. Alunos",
         "Turmas": "Qtd. Turmas",
     })
